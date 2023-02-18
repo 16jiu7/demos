b1c9f1871bbc966c7130780a23ece9f72f9293da
16jiu7 <2059865058@qq.com>
2023-02-18 21:03:47 +0800

    add pre-training

--- code/data_handeler.py
+++ code/data_handeler.py
@@ -9,6 +9,7 @@ import os
 import skimage.io as io
 from skimage.util import img_as_float32, img_as_ubyte
 import numpy as np
+from skimage.measure import regionprops
 
 # GLOBAL SETTINGS
 DRD = os.path.join('..', '4_retinal_datasets') # Dataset Root Dir
@@ -19,27 +20,42 @@ STARE_SPLIT = (8, 2, 10)
 
 class single_data():
     # class to store everything about a single data "point"
-    def __init__(self, ID, ori, fov_mask, gt, split):
+    def __init__(self, ID, ori, fov_mask, gt, split, cropped):
         self.ID = ID
         self.ori = ori
-        self.fov_mask = fov_mask
+        self.fov_mask = fov_mask if fov_mask.ndim == 2 else fov_mask[:, :, 0] # note that hrf's fov mask has ndim 3
         self.gt = gt
         assert split in ['training', 'val','test']
         self.split = split
+        self.cropped = cropped # crop to fov if True
+        self.shape = self.fov_mask.shape # shape by H, W
+        self.bbox = None # added by crop2fov function 
         self.pred = None
-        self.shape = (None, None)
+        self.cropped_ori, self.cropped_gt, self.cropped_fov_mask = self.crop2fov()
+        if self.cropped:
+            self.ori, self.gt, self.fov_mask = self.cropped_ori, self.cropped_gt, self.cropped_fov_mask
+            
+    def crop2fov(self):
+        bbox = regionprops(label_image = self.fov_mask)[0].bbox
+        assert len(bbox) == 4, 'error: fov_mask.ndim > 2'
+        self.bbox = bbox
+        return self.ori[bbox[0]:bbox[2], bbox[1]:bbox[3], :], \
+               self.gt[bbox[0]:bbox[2], bbox[1]:bbox[3]],\
+               self.fov_mask[bbox[0]:bbox[2], bbox[1]:bbox[3]]
 
 class RetinalDataset():
     # class whos instance is a retinal dataset like DRIVE = RetinalDataset(...)
-    def __init__(self, name, visualize = False):
+    def __init__(self, name, visualize = False, cropped = True):
         assert name in ['DRIVE', 'CHASEDB', 'HRF', 'STARE'], 'name must be in DRIVE, CHASEDB, HRF, STARE, got f{name}'
         self.name = name
+        self.cropped = cropped
         self.all_data = self.get_data()
         self.all_training, self.all_val, self.all_test = self.split_data()
-        self.visualize('./images/') if visualize else None
+        self.visualize('../images/') if visualize else None
+        
              
     def get_data(self):
-        data_list = eval('get_' + self.name + '(self)')
+        data_list = eval('get_' + self.name + f'(self, {self.cropped})')
         
         return data_list
     
@@ -54,11 +70,10 @@ class RetinalDataset():
         for data in self.all_data:
             gt = np.stack([data.gt]*3, axis = -1) if data.gt.ndim == 2 else data.gt
             fov_mask = np.stack([data.fov_mask]*3, axis = -1) if data.fov_mask.ndim == 2 else data.fov_mask
-            ori = data.ori
-            image = np.concatenate([ori, gt, fov_mask], axis = 0)
+            image = np.concatenate([data.ori, gt, fov_mask], axis = 0)
             io.imsave(os.path.join(save_dir, f'{self.name}_{data.ID}_{data.split}.png'), img_as_ubyte(image))
     
-def get_DRIVE(self):
+def get_DRIVE(self, cropped):
     all_DRIVE = []
     img_names = os.listdir(os.path.join(DRD, 'DRIVE', 'images'))
     img_names = sorted(img_names, key = lambda x : x.split(os.sep)[-1].split('_')[0])
@@ -71,13 +86,13 @@ def get_DRIVE(self):
             fov_mask = io.imread(os.path.join(DRD, 'DRIVE', 'masks', f'{ID}_mask.gif'))
             gt = io.imread(os.path.join(DRD, 'DRIVE', 'manual', f'{ID}_manual1.gif'))
             pred = io.imread(os.path.join(DRD, 'DRIVE', 'preds', f'{ID}.png'))
-            drive_instance = single_data(ID, ori, fov_mask, gt, split)
+            drive_instance = single_data(ID, ori, fov_mask, gt, split, cropped)
             drive_instance.pred = pred
             all_DRIVE.append(drive_instance)
             
     return all_DRIVE    
         
-def get_CHASEDB(self):
+def get_CHASEDB(self, cropped):
     all_CHASEDB = []
     img_names = os.listdir(os.path.join(DRD, 'CHASEDB', 'images'))
     img_names = sorted(img_names)
@@ -90,13 +105,13 @@ def get_CHASEDB(self):
             fov_mask = io.imread(os.path.join(DRD, 'CHASEDB', 'masks', f'Image_{ID}.gif'))
             gt = io.imread(os.path.join(DRD, 'CHASEDB', 'manual', f'Image_{ID}_1stHO.png'))
             pred = io.imread(os.path.join(DRD, 'CHASEDB', 'preds', f'{ID}.png'))
-            chasedb_instance = single_data(ID, ori, fov_mask, gt, split)
+            chasedb_instance = single_data(ID, ori, fov_mask, gt, split, cropped)
             chasedb_instance.pred = pred
             all_CHASEDB.append(chasedb_instance)
         
     return all_CHASEDB      
 
-def get_HRF(self):
+def get_HRF(self, cropped):
     all_HRF = []
     img_names = os.listdir(os.path.join(DRD, 'HRF', 'images'))
     img_names = sorted(img_names)
@@ -111,12 +126,12 @@ def get_HRF(self):
             fov_mask = io.imread(os.path.join(DRD, 'HRF', 'masks', f'{ID}.png'))
             gt = io.imread(os.path.join(DRD, 'HRF', 'manual1', f'{ID}.png'))
             pred = io.imread(os.path.join(DRD, 'HRF', 'preds', f'{ID}.png'))
-            HRF_instance = single_data(ID, ori, fov_mask, gt, split)
+            HRF_instance = single_data(ID, ori, fov_mask, gt, split, cropped)
             HRF_instance.pred = pred
             all_HRF.append(HRF_instance) 
         
     return all_HRF  
-def get_STARE(self):
+def get_STARE(self, cropped):
     all_STARE = []
     img_names = os.listdir(os.path.join(DRD, 'STARE', 'images'))
     img_names = sorted(img_names)
@@ -129,24 +144,23 @@ def get_STARE(self):
             fov_mask = io.imread(os.path.join(DRD, 'STARE', 'masks', f'{ID}.gif'))
             gt = io.imread(os.path.join(DRD, 'STARE', 'labels-ah', f'{ID}.ah.pgm'))
             pred = io.imread(os.path.join(DRD, 'STARE', 'preds', f'{ID}.png'))
-            stare_instance = single_data(ID, ori, fov_mask, gt, split)
+            stare_instance = single_data(ID, ori, fov_mask, gt, split, cropped)
             stare_instance.pred = pred
             all_STARE.append(stare_instance)
         
     return all_STARE         
     
     
-# if __name__ == '__main__':
-#     drive = RetinalDataset('DRIVE')
-#     chasedb = RetinalDataset('CHASEDB')
-#     hrf = RetinalDataset('HRF')
-#     stare = RetinalDataset('STARE')
-#     drive_all = drive.all_data
+#if __name__ == '__main__':
+#     drive = RetinalDataset('DRIVE', visualize=False, cropped = True)
     
-        
-        
+#     # chasedb = RetinalDataset('CHASEDB', visualize=True)
+     #hrf = RetinalDataset('HRF', visualize=False).all_data
+
+#     # stare = RetinalDataset('STARE', visualize=True)
+#     drive_1 = drive.all_data[0]
     
-    
+
     
     
     
