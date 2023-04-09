94dbccdc4f8ac14ddf695bbc4873334becdd28a0
16jiu7 <2059865058@qq.com>
2023-04-09 21:46:17 +0800

    add bbox marking

--- code/models/GAT.py
+++ code/models/GAT.py
@@ -6,6 +6,38 @@ from torch.autograd import Variable
 import torch.nn as nn
 import torch
 
+class RetinalGAT(nn.Module):
+    def __init__(self, num_of_layers, num_heads_per_layer, num_features_per_layer, add_skip_connection=True, bias=True,
+                 dropout=0.6, log_attention_weights=False):
+        super.__init__()
+        assert num_of_layers == len(num_heads_per_layer) == len(num_features_per_layer) - 1, f'Enter valid arch params.'
+
+        num_heads_per_layer = [1] + num_heads_per_layer  # trick - so that I can nicely create GAT layers below
+
+        gat_layers = []  # collect GAT layers
+        for i in range(num_of_layers):
+            layer = GATLayer(
+                num_in_features=num_features_per_layer[i] * num_heads_per_layer[i],  # consequence of concatenation
+                num_out_features=num_features_per_layer[i+1],
+                num_of_heads=num_heads_per_layer[i+1],
+                concat=True if i < num_of_layers - 1 else False,  # last GAT layer does mean avg, the others do concat
+                activation=nn.ELU() if i < num_of_layers - 1 else None,  # last layer just outputs raw scores
+                dropout_prob=dropout,
+                add_skip_connection=add_skip_connection,
+                bias=bias,
+                log_attention_weights=log_attention_weights
+            )
+            gat_layers.append(layer)
+
+        self.gat_net = nn.Sequential(*gat_layers)
+        
+    def forward(self, feats, graph):
+        pass
+
+
+
+        
+
 class GAT(torch.nn.Module):
     """
     The most interesting and hardest implementation is implementation #3.
--- code/new_mkgraph_method.py
+++ code/new_mkgraph_method.py
@@ -8,7 +8,7 @@ Created on Wed Mar 29 16:27:15 2023
 from skimage.segmentation import slic
 from skimage.segmentation import mark_boundaries
 from skimage.measure import regionprops, label
-from skimage.util import img_as_ubyte, img_as_bool
+from skimage.util import img_as_ubyte, img_as_bool, img_as_float32
 from skimage.filters import threshold_minimum, threshold_otsu
 from skimage import draw
 from skimage.morphology import remove_small_objects
@@ -19,8 +19,14 @@ import torch
 from data_handeler import RetinalDataset
 import datetime
 import networkx as nx
+from torchvision.transforms import ToTensor
+from torch.utils.data import Dataset, DataLoader
+from models.GAT import GAT
+import random
+from torch.autograd import Variable
 
-def get_bboxes_from_pred(pred, params = {'threshold': 'otsu', 'neg_iso_size': 32, 'n_slic_pieces': 800}):
+
+def get_bboxes_from_pred(pred, params = {'threshold': 'otsu', 'neg_iso_size': 6, 'n_slic_pieces': 800}):
     # larger neg_iso_size, less node, other params don't matter a lot
     assert pred.dtype == np.uint8
     if params['threshold'] == 'otsu':
@@ -59,7 +65,7 @@ def get_bboxes_from_pred(pred, params = {'threshold': 'otsu', 'neg_iso_size': 32
         h, w = x[2] - x[0], x[3] - x[1]
         if h < 17 or w < 17:
             center = (int((x[2] + x[0]) / 2), int((x[3] + x[1]) / 2))
-            small_bboxes[idx] = (center[0] - 8, center[1] - 8, center[0] + 8, center[1] + 8)
+            small_bboxes[idx] = (center[0] - 8, center[1] - 8, center[0] + 9, center[1] + 9)
             
     for x in large_bboxes: #split large bboxes
         h, w = x[2] - x[0], x[3] - x[1]
@@ -68,7 +74,7 @@ def get_bboxes_from_pred(pred, params = {'threshold': 'otsu', 'neg_iso_size': 32
         for i in range(h_n):
             for j in range(w_n):
                 top_left = (x[0] + (i + 1) * 17, x[1] + (j + 1) * 17)
-                sub_bboxes.append((top_left[0], top_left[1], top_left[0] + 17, top_left[1] + 17))
+                sub_bboxes.append((top_left[0], top_left[1], top_left[0] + 18, top_left[1] + 18))
         small_bboxes += sub_bboxes
     # remove bboxes out of range    
     small_bboxes = list(filter(lambda x: (x[0]>=0) and (x[1]>=0) and(x[2]<=pred.shape[0]) and (x[3]<=pred.shape[1]), small_bboxes))
@@ -120,9 +126,10 @@ def add_edges(bboxes, narrow = 100, n_links = 2):
             edges = [(node, target) for target in targets] 
             graph.add_edges_from(edges)  
         
-    return graph    
+    return nx.DiGraph(graph)    
 
 def vis_bbox_graph(bboxes, graph, background, save_dir):
+    #vis_bbox_graph(bboxes, graph, np.stack([certain_pred]*3, axis = -1), save_dir = f'{data.ID}_bbox_vis.png')
     for bbox in bboxes:
         start = (bbox[0], bbox[1])
         end = (bbox[2], bbox[3])
@@ -136,28 +143,115 @@ def vis_bbox_graph(bboxes, graph, background, save_dir):
         draw.set_color(background, (rr, cc), color = (0,0,255))
     io.imsave(save_dir, img_as_ubyte(background))
 
-params_for_stare = {'threshold': 'otsu', 'neg_iso_size': 16, 'n_slic_pieces': 500}
-params_for_drive = {'threshold': 'otsu', 'neg_iso_size': 16, 'n_slic_pieces': 500}
+def bbox_graph_to_torch(graph, input_feats):
+    # get node feats, transform edge list to torch tensor
+    N,C,H,W = input_feats.shape
+    assert N == 1 
+    node_feats = []
+    for node in graph.nodes: # {'center': (90, 374), 'bbox': (82, 366, 98, 382)}
+        x, y = graph.nodes[node]['center']
+        patch = input_feats[:,:,x-8:x+9,y-8:y+9]
+        node_feats.append(patch.flatten().view(1, -1)) # 1,C,H,W -> C*H*W
+    node_feats = torch.cat(node_feats, dim = 0)    
+    edges = torch.Tensor([edge for edge in graph.edges]).long().view(2, -1)
+    return node_feats, edges
 
-datas = RetinalDataset('STARE').all_data
-for data in datas:
-    pred = data.pred
-    mask = data.fov_mask.astype(bool)
-    pred = pred * mask
-    bboxes, certain_pred = get_bboxes_from_pred(pred)
-    graph = add_edges(bboxes)
-    vis_bbox_graph(bboxes, graph, np.stack([certain_pred]*3, axis = -1), save_dir = f'{data.ID}_bbox_vis.png')
-    print(graph)
-    print(nx.number_connected_components(graph), '\n')        
+class TrainDataset(Dataset):
+    def __init__(self, dataset_name, split, transforms = None, color_jitter = None):
+        self.split = split
+        self.dataset_name = dataset_name
+        self.transforms = transforms
+        self.color_jitter = color_jitter
+        if split == 'train':
+            self.data = RetinalDataset(self.dataset_name, cropped = True).all_training
+        elif split == 'val':
+            self.data = RetinalDataset(self.dataset_name, cropped = True).all_val
 
+    def __len__(self):
+        return len(self.data)
 
+    def __getitem__(self, idx):
+        data = self.data[idx]
+        return data
 
+def my_collate_fn(batch):
+    return batch[0]
 
+def setup_random_seed(seed):
+     torch.manual_seed(seed)
+     torch.cuda.manual_seed_all(seed)
+     np.random.seed(seed)
+     random.seed(seed)
+     torch.backends.cudnn.deterministic = True
+     
+def place_bbox_back(logits, pred, graph):
+    # pred.shape == 1,1,H,W
+    logits = logits.view(logits.shape[0], 17, 17)
+    for idx, node in enumerate(graph.nodes()):
+        c_x, c_y = graph.nodes[node]['center']
+        pred[:,:,c_x-8 : c_x+9, c_y-8 : c_y+9] = logits[idx, :, :]    
+    return pred    
+# In[]
+setup_random_seed(100)
+TRAIN_DATASET = 'DRIVE'
+criterion = torch.nn.BCEWithLogitsLoss()
+gnn = GAT(3, [4,4,4], [17*17*4, 17*17, 17*17, 17*17], dropout = 0.2)
+optimizer= torch.optim.Adam(gnn.parameters(), lr = 1e-3, weight_decay = 0)
+n_epoch = 150
+device = 'cuda'
+gnn = gnn.to(device)
 
+train_set = TrainDataset(dataset_name = TRAIN_DATASET, split = 'train')
+val_set = TrainDataset(dataset_name = TRAIN_DATASET, split = 'val')   
+train_loader = DataLoader(train_set, batch_size = 1, num_workers = 0, shuffle = True, collate_fn = my_collate_fn)
+val_loader = DataLoader(val_set, batch_size = 1, num_workers = 0, shuffle = False, collate_fn = my_collate_fn)
 
-
-
-
+for epoch in range(n_epoch):
+    epoch_loss = 0
+    for data in train_loader:
+        pred = data.pred
+        mask = data.fov_mask.astype(bool)
+        pred = pred * mask
+        bboxes, certain_pred = get_bboxes_from_pred(pred)
+        graph = add_edges(bboxes)
+        ori = ToTensor()(data.ori).unsqueeze(0).to(device)
+        pred_tensor = ToTensor()(data.pred).unsqueeze(0).to(device)
+        node_feats, edges = bbox_graph_to_torch(graph, input_feats = torch.cat([ori, pred_tensor], dim = 1))
+        node_feats, edges = node_feats.to(device), edges.to(device)
+        logits, _ = gnn((node_feats, edges), with_feats = False)
+        gt = ToTensor()(data.gt).unsqueeze(0).to(device)
+        #gts, _ = bbox_graph_to_torch(graph, gt)
+        reconstructed = place_bbox_back(logits, pred_tensor, graph)
+        #reconstructed, gt = reconstructed.view(1,-1), gt.view(1,-1)
+        loss = criterion(reconstructed, gt)
+        
+        epoch_loss += loss.item()
+        loss.backward()
+        optimizer.step()
+        optimizer.zero_grad()
+    epoch_mean_loss = epoch_loss / len(train_loader)
+    print(f'epoch {epoch+1}, loss {epoch_mean_loss:.4f}')
+    
+    for data in val_loader:
+        with torch.no_grad():
+            for data in val_loader:
+                pred = data.pred
+                mask = data.fov_mask.astype(bool)
+                pred = pred * mask
+                bboxes, certain_pred = get_bboxes_from_pred(pred)
+                graph = add_edges(bboxes)
+                ori = ToTensor()(data.ori).unsqueeze(0).to(device)
+                pred_tensor = ToTensor()(data.pred).unsqueeze(0).to(device)
+                node_feats, edges = bbox_graph_to_torch(graph, input_feats = torch.cat([ori, pred_tensor], dim = 1))
+                node_feats, edges = node_feats.to(device), edges.to(device)
+                logits, _ = gnn((node_feats, edges), with_feats = False)
+                gt = ToTensor()(data.gt).unsqueeze(0).to(device)
+                #gts, _ = bbox_graph_to_torch(graph, gt)
+                s_logits = torch.nn.Sigmoid()(logits)
+                reconstructed = place_bbox_back(s_logits, pred_tensor, graph)
+                outputs = reconstructed[0][0].cpu().numpy()
+                io.imsave(f'{data.ID}_gnn_pred.png', img_as_ubyte(outputs))
+del gnn
 
 
 
